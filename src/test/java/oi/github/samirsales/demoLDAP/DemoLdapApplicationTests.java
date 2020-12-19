7a021e4a58faadbf7a3f2af1255681c6ec977249
Samir Sales <samir.sribeiro@gmail.com>
2020-12-19 11:39:39 -0300

    first commit

---
+++ src/test/java/oi/github/samirsales/demoLDAP/DemoLdapApplicationTests.java
@@ -0,0 +1,13 @@
+package oi.github.samirsales.demoLDAP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoLdapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
